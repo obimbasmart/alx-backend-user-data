0f22c93dfbbb6b1b0f5ff74d94c0c0b9c2dfe4d9
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-13 20:30:32 +0100

    fix: logical error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -30,10 +30,11 @@ class RedactingFormatter(logging.Formatter):
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.__fields = fields
+        logging.basicConfig(format=RedactingFormatter.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
         """obscure values in incoming log records"""
-        return filter_datum(self.__fields, self.REDACTION,
-                            record.getMessage(),
-                            self.SEPARATOR
-                            )
+        return filter_datum(self.__fields,
+                            self.REDACTION,
+                            super().format(record),
+                            self.SEPARATOR)
