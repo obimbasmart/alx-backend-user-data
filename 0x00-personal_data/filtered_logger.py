dfd9bd465f9eb44b3730050498c9a5ddbf8d5daf
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-13 22:43:41 +0100

    fix: type annotation: func return type

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -33,7 +33,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD', 'root')
     username = os.getenv('PERSONAL_DATA_DB_USERNAME', '')
     host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
