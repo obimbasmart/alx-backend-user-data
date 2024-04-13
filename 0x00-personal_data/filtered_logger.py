fffb75710c5cd8643d4b7b811b3386e00027d0e5
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-13 18:58:18 +0100

    add: implement log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 
 """Regex-ing: obfuscated personal identifiable data"""
 
+import logging
 from typing import (List)
 import re
 
@@ -16,3 +17,23 @@ def filter_datum(fields: List[str], redaction: str,
                          r'\1{}{}'.format(redaction, separator),
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.__fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """obscure values in incoming log records"""
+        return filter_datum(self.__fields, self.REDACTION,
+                            record.getMessage(),
+                            self.SEPARATOR
+                            )
