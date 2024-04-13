bfc135e48b3dc91bd98a3fcf2af322fb209a46f9
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-13 18:40:30 +0100

    fix: typo error in argument name

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,12 +7,12 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, seperator: str) -> str:
+                 message: str, separator: str) -> str:
     """
     Obscure personal data by replacing each field with @redaction
     """
     for field in fields:
-        message = re.sub(r'({}=).*?{}'.format(field, seperator),
-                         r'\1{}{}'.format(redaction, seperator),
+        message = re.sub(r'({}=).*?{}'.format(field, separator),
+                         r'\1{}{}'.format(redaction, separator),
                          message)
     return message
