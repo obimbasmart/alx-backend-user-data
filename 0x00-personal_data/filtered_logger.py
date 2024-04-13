ad3f0f95522ed24714786b02b3c9ab259dba6b0c
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-13 18:39:04 +0100

    fix: use correct argument name

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,12 +7,12 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 msg: str, seperator: str) -> str:
+                 message: str, seperator: str) -> str:
     """
     Obscure personal data by replacing each field with @redaction
     """
     for field in fields:
-        msg = re.sub(r'({}=).*?{}'.format(field, seperator),
-                     r'\1{}{}'.format(redaction, seperator),
-                     msg)
-    return msg
+        message = re.sub(r'({}=).*?{}'.format(field, seperator),
+                         r'\1{}{}'.format(redaction, seperator),
+                         message)
+    return message
