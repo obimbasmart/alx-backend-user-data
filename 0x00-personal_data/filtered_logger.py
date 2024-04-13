e8f698e9ef6328a3f3e901be58501a76e4ca4443
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-13 18:28:39 +0100

    add: filtered logger: 0x00

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""Regex-ing: obfuscated personal identifiable data"""
+
+from typing import (List)
+import re
+
+
+def filter_datum(fields: List, redaction: str,
+                 msg: str, seperator: str) -> str:
+    """
+    Obscure personal data
+
+    Args:
+        @fields: list representing all fields to obfuscate
+        @redaction: what the field will be obfuscated with
+        @msg: log line
+        @seperator: character separating all fields in the log line
+    """
+    for field in fields:
+        msg = re.sub(r'({}=).*?{}'.format(field, seperator),
+                     r'\1xxx;',
+                     msg)
+    return msg
