37ef28bd8951e3fbc8cb48a1bf4ad182b3199081
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-14 21:25:45 +0100

    add: check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """encrypt password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """----- ----- ----"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
