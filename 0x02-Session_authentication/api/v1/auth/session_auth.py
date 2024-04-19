7911847c2893ac3499908431bd1a83b51a682119
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-19 23:52:02 +0100

    add: get user_id from session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -22,3 +22,9 @@ class SessionAuth(BasicAuth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """get user_id from session"""
+        if not session_id or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
