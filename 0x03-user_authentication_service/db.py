849d6dc2685d98fddb17378b32720a3b1e3beb57
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-29 21:41:22 +0100

    fix: add function annotation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """add user"""
         user = User()
         user.email = email
