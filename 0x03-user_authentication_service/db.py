982a519d36f1bae72e31717037b4e0a90ab93408
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-29 21:07:01 +0100

    add: add_user func

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,43 @@
+# /usr/bin/env python3
+
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from uuid import uuid4
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email, hashed_password):
+        """add user"""
+        user = User()
+        user.email = email
+        user.hashed_password = hashed_password
+        user.session_id = uuid4().hex
+        user.reset_token = '112'
+        self._session.add(user)
+        self._session.commit()
+        return user
