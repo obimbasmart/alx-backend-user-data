02c6ab9ea542848d759e918e39fb903ad201988a
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-05-06 08:49:52 +0100

    fix: new users should not have resert token ::02

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -15,4 +15,4 @@ class User(Base):
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True, default=None)
