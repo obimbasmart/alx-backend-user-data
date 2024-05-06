f67b2d1ff1bdc5503913da309413af3ff7d1d1ae
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-05-06 11:13:00 +0100

    test: add unittest

---
+++ 0x03-user_authentication_service/main.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""End to end integration test"""
+
+
+def register_user(email: str, password: str) -> None:
+    pass
+
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    pass
+
+
+def log_in(email: str, password: str) -> str:
+    return "dummy"
+
+
+def profile_unlogged() -> None:
+    pass
+
+
+def profile_logged(session_id: str) -> None:
+    pass
+
+
+def log_out(session_id: str) -> None:
+    pass
+
+
+def reset_password_token(email: str) -> str:
+    return "dummy"
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    pass
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
